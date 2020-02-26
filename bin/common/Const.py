1c5e89f463a05df70bb602de8406bad0c7f6ced0
Михаил Немчинов <m.nemchinov@autorus.ru>
2020-02-26 10:11:41 +0300

    Created readme

--- bin/common/Const.py
+++ bin/common/Const.py
@@ -1,6 +1,6 @@
 # region bin
 
-APP_NAME = "Flask Rest Api Sceleton"
+APP_NAME = "Flask Rest Api Skeleton"
 APP_SHORT_NAME = "FRAS"
 APP_DESCRIPTION = "Skeleton of the application for developing the REST API of the service"
 APP_VERSION = "0.1.1 (Alpha)"
